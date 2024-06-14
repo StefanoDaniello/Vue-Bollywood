98750e45bc90a8a65d9ca21ff57f8220780254d0
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-14 18:47:40 +0200

    created AboutComponent

---
+++ src/pages/AboutComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        About Component
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AboutComponent",
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
