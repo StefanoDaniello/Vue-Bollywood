cee16ee09433e4589acc12134d3b61db33380747
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-14 18:48:02 +0200

    update with hero image

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,15 +1,23 @@
 <template>
+  <div class="container herocontainer">
+    <div id="hero" class="container"></div>
+  </div>
   <div class="container">
-    <div class="debug">Questo è il AppHome.vue</div>
+    <CardComponent :item="movies" />
   </div>
 </template>
 
 <script>
 import { store } from "../store";
 import axios from "axios";
+import CardComponent from '../components/CardComponent.vue'
+
 
 export default {
   name: "AppHome",
+  components: {
+        CardComponent
+    },
   data() {
     return {
       store,
@@ -32,5 +40,17 @@ export default {
 <style lang="scss" scoped>
 .debug {
   border: 1px solid red;
+}
+.herocontainer{
+  width: 800px;
+  height: 500px;
+  #hero{
+    background-image: url('../img/heroImgtr.png');
+    width: 100%;
+    height: 100%;
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: 0 auto;
+  }
 }
 </style>
