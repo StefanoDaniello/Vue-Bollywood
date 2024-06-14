1797c26ce3c02ad3e94e175782730df112fca7cf
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-14 18:46:09 +0200

    update routes

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
+import ContactComponent from './pages/ContactComponent.vue';
+import AboutComponent from './pages/AboutComponent.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -8,7 +10,17 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: AppHome,
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: ContactComponent,
         },        
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutComponent,
+        },
     ]
 });
 export { router };
