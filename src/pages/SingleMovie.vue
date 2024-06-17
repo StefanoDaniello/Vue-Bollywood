f32c3b52d842618b3959961ea22e01fa3d2473c8
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-17 15:58:19 +0200

    add review

--- src/pages/SingleMovie.vue
+++ src/pages/SingleMovie.vue
@@ -19,16 +19,34 @@
         </div>
         <h2>Recensioni</h2>
 
-       <div class="row">
-           <div class="col-6" v-for="(item, index) in movie.reviews" :key="index">
-              <div>{{ item.author }}</div>
+
+        <div class="row d-flex justify-content-center">
+            <div class="col-2 mb-3" v-for="(review, index) in movie.reviews" :key="index">
+                <div class="card" id="reviews" >
+                    <div class="d-flex">
+                        <img 
+                        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" 
+                        class="rounded-circle m-3" 
+                        alt="Author image" 
+                        width="80" 
+                        height="80"
+                        />
+                        <h5 class="card-title title">{{ review.author }}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">{{ review.content }}</p>
+                    </div>
+                    <p>
+                        <small class="text-muted">
+                        <span v-for="star in 5" :key="star" class="fa fa-star" :class="{'checked': star <= review.rating}"></span>
+                        </small>
+                    </p>
+                    <div class="card-footer">Created at: {{ formatDate(review.created_at) }}</div>
+                </div>
             </div>
-       </div> 
+        </div>
 
     </div>
-  
-    
-   
 </template>
 
 <script>
@@ -64,6 +82,9 @@
             },
             setDefaultImage(event) {
             event.target.src = this.store.defaultImg
+            },
+            formatDate(dateString) {
+                return dateString.split(' ')[0];
             }
         },
         mounted() {
@@ -79,83 +100,109 @@
 </script>
 
 <style lang="scss" scoped>
+    // .postcard {
+    // border: 1px solid #ccc;
+    // margin-bottom: 10px;
+    // padding: 10px;
+    // }
+    // .postcard__img {
+    // width: 100%;
+    // height: auto;
+    // }
+    .fa-star {
+    color: #ddd;
+    }
+    .fa-star.checked {
+    color: #ffc107;
+    }
+    #reviews {
+        min-height: 400px;
+        width: 300px;
+        border-radius: 20px;
+    }
+    .title {
+        margin: 45px 0 0 20px;
+    }
+
+    
 .container{
     padding: 20px; 
     display: flex;
     justify-content: center;
 }
-    .card-container {
-  width: 700px;
-  height: 700px;
-  position: relative;
-  border-radius: 10px;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-  overflow: hidden;
+
+.card-container {
+    width: 700px;
+    height: 700px;
+    position: relative;
+    border-radius: 10px;
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
 }
 
 .card {
-  width: 100%;
-  height: 100%;
-  border-radius: inherit;
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
 }
 
 .card .front-content {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1)
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1)
 }
 
 .card .front-content p {
-  font-size: 32px;
-  font-weight: 700;
-  opacity: 1;
-  background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100% );
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1)
+    font-size: 32px;
+    font-weight: 700;
+    opacity: 1;
+    background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100% );
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1)
 }
 
 .card .content {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  gap: 10px;
-  background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100% );
-  color: #e8e8e8;
-  padding: 20px;
-  line-height: 1.5;
-  border-radius: 5px;
-  pointer-events: none;
-  transform: translateX(-96%);
-  transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    gap: 10px;
+    background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100% );
+    color: #e8e8e8;
+    padding: 20px;
+    line-height: 1.5;
+    border-radius: 5px;
+    pointer-events: none;
+    transform: translateX(-96%);
+    transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1);
 }
 
 .card .content .heading {
-  font-size: 32px;
-  font-weight: 700;
+    font-size: 32px;
+    font-weight: 700;
 }
 
 .card:hover .content {
-  transform: translateY(0);
+    transform: translateY(0);
 }
 
 .card:hover .front-content {
-  transform: translateX(-30%);
+    transform: translateX(-30%);
 }
 
 .card:hover .front-content p {
-  opacity: 0;
+    opacity: 0;
 }
 
 
