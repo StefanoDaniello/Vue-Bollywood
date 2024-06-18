edc805d57583cbc630d0612b41202cb115c1c9db
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-18 10:50:49 +0200

    Revert "minor fix"
    This reverts commit 1c731d4db08d2811eb2333cdd1a0ee2885903be9.

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="container herocontainer">
-    <div id="hero"></div>
+    <div id="hero" class="container"></div>
   </div>
   <div class="container">
     <div class="text-center"><h1>Proiezioni in corso</h1></div>
-    <div class="row ">
-      <div v-for="(item, index) in movies" :key="index" class="col-12 col-xl-4 col-lg-6 ">
-          <CardComponent1 :item="item" />
-      </div>
-    </div>
+    <div v-for="(item, index) in movies" :key="index">
+      <CardComponent :item="item" />
+    </div> 
   </div>
 </template>
 
 <script>
 import { store } from "../store";
 import axios from "axios";
-import CardComponent1 from "../components/CardComponent1.vue";
+import CardComponent from '../components/CardComponent.vue'
+
 
 export default {
   name: "AppHome",
   components: {
-    CardComponent1,
-  },
+      CardComponent
+    },
   data() {
     return {
       store,
@@ -30,17 +29,16 @@ export default {
   },
   methods: {
     getmovies() {
-      axios
-        .get(this.store.apiBaseUrl + "/movies")
-        .then((response) => {
-          // console.log(response.data.results);
-          this.movies = response.data.results;
-          // console.log(this.movies);
-        })
-        .catch((error) => {
-          console.error("Errore nella richiesta:", error);
-        });
-    },
+      axios.get(this.store.apiBaseUrl + "/movies")
+    .then((response) => {
+        // console.log(response.data.results);
+        this.movies = response.data.results;
+        // console.log(this.movies);
+    })
+    .catch((error) => {
+        console.error("Errore nella richiesta:", error);
+    });
+    }
   },
   mounted() {
     this.getmovies();
@@ -52,16 +50,16 @@ export default {
 .debug {
   border: 1px solid red;
 }
-.herocontainer {
+.herocontainer{
   width: 800px;
   height: 500px;
-  #hero {
-    background-image: url("../img/heroImgtr.png");
-    // width: 100%;
+  #hero{
+    background-image: url('../img/heroImgtr.png');
+    width: 100%;
     height: 100%;
-    background-size: cover;
+    background-size: contain;
     background-repeat: no-repeat;
-    margin: 65px auto;
+    margin: 0 auto;
   }
 }
 </style>
