8350974d0fa08ee07cc0133baf74dc8e81ce965f
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-18 14:29:01 +0200

    fix date

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,43 +1,34 @@
 <template>
-  <div class="container herocontainer ">
-    <div>CIAO</div>
+  <div class="container herocontainer">
     <div id="hero" class="container"></div>
   </div>
   <div class="container">
- 
-
     <div class="d-flex justify-content-between align-items-center">
       <h1>Seleziona la data </h1>
-      <select name="movies" id="movies" class="form-select w-25" 
-        @change="setParams()" v-model="data">
+      <select name="movies" id="movies" class="form-select w-25" @change="setParams()" v-model="data">
         <option value="" selected>Seleziona la data</option>
         <option :value="currentDate">Oggi</option>
-        <option :value="item.date" v-for="item in store.data" :key="data.id">{{ item.date }}
-        </option>
+        <option v-for="date in filteredDates" :key="date" :value="date">{{ date }}</option>
       </select>
-  </div>
-
+    </div>
 
     <div class="text-center"><h1>Proiezioni in corso</h1></div>
-    <div class="row">
-      <div v-for="(item, index) in movies" :key="index" class="col-12 col-xl-4 col-lg-6">
-        <CardComponent1 :item="item" />
-      </div> 
-    </div>
+    <div v-for="(item, index) in movies" :key="index">
+      <CardComponent :item="item" />
+    </div> 
   </div>
 </template>
 
 <script>
 import { store } from "../store";
 import axios from "axios";
-import CardComponent1 from '../components/CardComponent1.vue'
-
+import CardComponent from '../components/CardComponent.vue';
 
 export default {
   name: "AppHome",
   components: {
-      CardComponent1
-    },
+    CardComponent
+  },
   data() {
     return {
       store,
@@ -56,41 +47,38 @@ export default {
       return `${year}-${month}-${day}`; // Formato YYYY-MM-DD
     },
     setParams() {
-        if(this.data){
-          this.params.data = this.data
-        }
-        this.getmovies();
+      if (this.data) {
+        this.params.data = this.data;
+      }
+      this.getMovies();
     },
-    getmovies() {
-      axios.get(this.store.apiBaseUrl + "/movies", {
-        params: this.params
-      })
-    .then((response) => {
-        // console.log(response.data.results);
-        this.movies = response.data.results;
-        // console.log(this.movies);
-        this.params={};
-    })
-    .catch((error) => {
-        console.error("Errore nella richiesta:", error);
-    });
+    getMovies() {
+      axios.get(this.store.apiBaseUrl + "/movies", { params: this.params })
+        .then((response) => {
+          this.movies = response.data.results;
+          this.params = {};
+        })
+        .catch((error) => {
+          console.error("Errore nella richiesta:", error);
+        });
+    }
+  },
+  computed: {
+    uniqueDates() {
+      return new Set(this.store.data.map(item => item.date));
+    },
+    filteredDates() {
+      const datesArray = Array.from(this.uniqueDates);
+      // Filtra le date di oggi
+      return datesArray.filter(date => date !== this.currentDate);
     }
   },
   mounted() {
-    this.getmovies();
-  },
-  computed: {
-    selectedCategory() {
-      const proiezioni = this.store.data.find(movies => movies.id == this.data);
-      return proiezioni ? movies.date : 'category not found';
-    },
-    // selectedCategory() {
-    //   const category = this.store.categories.find(category => category.id == this.category);
-    //   return category ? category.name : 'category not found';
-    // }
-  },
+    this.getMovies();
+  }
 };
 </script>
+
 
 <style lang="scss" scoped>
 .debug {
@@ -100,12 +88,12 @@ export default {
   width: 800px;
   height: 500px;
   #hero{
-    background-image: url('../img/heroImgtr-def.png');
-    background-position: center;
+    background-image: url('../img/heroImgtr.png');
     width: 100%;
     height: 100%;
     background-size: contain;
     background-repeat: no-repeat;
+    margin: 0 auto;
   }
 }
 </style>
